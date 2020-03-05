e431b023e5e8dd45a3f4fcf107c6e4983b3447ff
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2020-03-05 23:12:00 +1000

    finished practice 1, started another exercise

---
+++ first-ml-model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#Path of file to read
+iowa_file_path = '../data/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+
+#Set up code checking
+# from learntools.core import binder
--- first.py
+++ first.py
@@ -13,3 +13,14 @@ X = melbourne_data[melbourne_features]
 
 print(X.describe())
 print(X.head())
+
+# Define model. Specify a number for random_state to ensure same results each run
+melbourne_model = DecisionTreeRegressor(random_state=1)
+
+#Fit model
+print(melbourne_model.fit(X, y))
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
